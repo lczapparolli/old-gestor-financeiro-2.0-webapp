b32da8f923e083bcd96a2de5bf0e7bdf3b24a0bb
Luis Carlos <lczapparolli@gmail.com>
2018-10-14 22:25:50 -0300

    Setting up eslint and react-router
    Configuring eslint

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './style/App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <div className="App">
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
