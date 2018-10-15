992fc7ed70b279f26efa27690a362adacca7f6be
Luis Carlos <lczapparolli@gmail.com>
2018-10-14 22:49:02 -0300

    Creating HomePage route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage.js';
 import './style/App.css';
 
 class App extends Component {
     render() {
         return (
             <Router>
-                <div className="App">
+                <div className="container">
+                    <Route exact path="/" component={HomePage} />
                 </div>
             </Router>
         );
