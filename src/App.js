5fe4aecd1b9829d5c601ecfd4820d8e4e9130783
Luis Carlos <lczapparolli@gmail.com>
2018-10-15 21:59:11 -0300

    Creating LoginPage route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
+//External libraries
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+//Pages
 import HomePage from './pages/HomePage.js';
+import LoginPage from './pages/LoginPage.js';
+//Style
 import './style/App.css';
 
 class App extends Component {
@@ -9,6 +13,7 @@ class App extends Component {
             <Router>
                 <div className="container">
                     <Route exact path="/" component={HomePage} />
+                    <Route path="/login" component={LoginPage} />
                 </div>
             </Router>
         );
