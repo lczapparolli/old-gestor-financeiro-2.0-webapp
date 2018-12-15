2ab4396ee17c158b9d954a11cc5d3aa7f57f5287
Luis Carlos <lczapparolli@gmail.com>
2018-12-15 15:13:07 -0200

    Adding fake-indexeddb for testing stored data
    - Added configuration to use mocked indexedDb when running tests

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,12 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
+import fakeIndexedDB from 'fake-indexeddb';
+import IDBKeyRange from 'fake-indexeddb/lib/FDBKeyRange';
+import Dexie from 'dexie';
+
 configure({ adapter: new Adapter() });
+
+//Mocked indexedDb
+Dexie.dependencies.indexedDB = fakeIndexedDB;
+Dexie.dependencies.IDBKeyRange = IDBKeyRange;
