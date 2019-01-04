3d150aac01439219a588450880b7ee3bbf023123
Luis Carlos <lczapparolli@gmail.com>
2019-01-03 23:29:41 -0200

    Creating format number helper

---
+++ src/helpers/FormatNumber.js
@@ -0,0 +1,47 @@
+
+/**
+ * Validates and convert input value to assure that is a number
+ * @param {String|Number} value Value to be formated
+ * @returns {Number} Converted value
+ * @throws {TypeError} Throws an exception when value is not a valid number
+ */
+function validate(value) {
+    if (typeof value === 'undefined' || value === null)
+        throw new TypeError('value required');
+    
+    const result = Number.parseFloat(value);
+
+    if (Number.isNaN(result))
+        throw new TypeError('value must be numeric');
+
+    return result;
+}
+
+/**
+ * Formats a number with 2 decimal places and thousands separator.
+ * This functions is adapted to brazilian format with comma ',' as decimal separator and dots '.' as thousand separator.
+ * @param {Number|String} value Number or numeric string to be formated
+ * @returns {String} Formated value
+ * @throws {TypeError} Throws an exception when value is not a valid number
+ */
+function formatNumber(value) {
+    value = validate(value);
+    const negative = value < 0;
+    const parts = Math.abs(value).toFixed(2).split('.');
+    let integer = parts[0];
+    const decimal = parts[1];
+
+    if (integer.length > 3) {
+        let position = integer.length % 3;
+        while (position < integer.length) {
+            integer = integer.slice(0, position) + '.' + integer.slice(position); 
+            position += 4; //+4 to include new added char
+        }
+    }
+
+    let result = (negative ? '-' : '') + integer + ',' + decimal;
+    
+    return result;
+}
+
+export default formatNumber;
---
+++ src/helpers/__tests__/FormatNumber.js
@@ -0,0 +1,41 @@
+//Libs
+import chai from 'chai';
+//Tested component
+import formatNumber from '../FormatNumber';
+
+const cExpect = chai.expect;
+
+describe('Number format helper', () => {
+    it('is a function', () => {
+        cExpect(formatNumber).to.be.a('function');
+    });
+
+    it('expects a number and a format destination', () => {
+        cExpect(() => formatNumber()).to.throw('value required');
+        cExpect(() => formatNumber('abc')).to.throw('value must be numeric');
+        cExpect(() => formatNumber(123)).to.not.throw();
+        cExpect(() => formatNumber('123')).to.not.throw();
+    });
+
+    it('returns a formated string with 2 decimal places and thousands separator', () => {
+        //Zero
+        cExpect(formatNumber(0)).to.be.equal('0,00');
+        //Positive numbers
+        cExpect(formatNumber(1)).to.be.equal('1,00');
+        cExpect(formatNumber(1.1)).to.be.equal('1,10');
+        cExpect(formatNumber(0.1)).to.be.equal('0,10');
+        //Negative numbers
+        cExpect(formatNumber(-1)).to.be.equal('-1,00');
+        cExpect(formatNumber(-1.1)).to.be.equal('-1,10');
+        cExpect(formatNumber(-0.1)).to.be.equal('-0,10');
+        //Thousands
+        cExpect(formatNumber(1000)).to.be.equal('1.000,00');
+        cExpect(formatNumber(-1000)).to.be.equal('-1.000,00');
+        //Milion
+        cExpect(formatNumber(1000000)).to.be.equal('1.000.000,00');
+        cExpect(formatNumber(-1000000)).to.be.equal('-1.000.000,00');
+        //More decimal places
+        cExpect(formatNumber(1.111)).to.be.equal('1,11');
+        cExpect(formatNumber(-1.111)).to.be.equal('-1,11');
+    });
+});
