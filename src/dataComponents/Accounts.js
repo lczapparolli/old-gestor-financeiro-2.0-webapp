884e78522145d2f2b668cf7e0909e77634aaa3cf
Luis Carlos <lczapparolli@gmail.com>
2019-01-10 23:50:14 -0200

    Adding AccountsController into component
    - Removed fixed data

--- src/dataComponents/Accounts.js
+++ src/dataComponents/Accounts.js
@@ -1,9 +1,11 @@
 //Libs
-import React, { Fragment } from 'react';
+import React, { Fragment, Component } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 //Style
 import '../style/Account.scss';
+//Controller
+import accountsController from '../controllers/AccountsController';
 //Components
 import GridRow from '../components/GridRow';
 import GridCell from '../components/GridCell';
@@ -13,69 +15,59 @@ import formatNumber from '../helpers/FormatNumber';
 /**
  * Component to list accounts with different types and sum the balance
  */
-function Accounts() {
-    //Mocked data
-    const accounts = [
-        { id: 1, name: 'BB - Carol', balance: 6.20, type: 'account' },
-        { id: 2, name: 'BB - Luis', balance: 26.54, type: 'account' },
-        { id: 3, name: 'Bradesco - Luis', balance: 1041.62, type: 'account' },
-        { id: 4, name: 'Dinheiro', balance: 63.00, type: 'account' },
+class Accounts extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            groups: null
+        };
+    }
 
-        { id: 5, name: 'Poupança BB - Luis', balance: 3027.68, type: 'invest' },
+    async componentDidMount() {
+        const groups = await accountsController.findAll();
+        this.setState({groups});
+    }
 
-        { id: 6, name: 'Crédito BB', balance: -1639.90, type: 'cc' },
-        { id: 7, name: 'Crédito Santander', balance: -1440.93, type: 'cc' }
-    ];
+    render() {
+        const { groups } = this.state;
+        if (!groups) return <h1>Loading</h1>;
 
-    let groups = {
-        total: 0,
-        account: { items: [], sum: 0 },
-        cc: { items: [], sum: 0 },
-        invest: { items: [], sum: 0 }
-    };
-
-    groups = accounts.reduce((group, account) => {
-        group[account.type].items.push(account);
-        group[account.type].sum += account.balance;
-        group.total += account.balance;
-        return group;
-    }, groups);
-
-    return (
-        <Fragment>
-            <GridRow>
-                <GridCell>
-                    <h2>Accounts</h2>
-                </GridCell>
-                <GridCell shrink>
-                    <Link to="/accounts/new">+</Link>
-                </GridCell>
-            </GridRow>
-            <GridRow>
-                <GridCell>
-                    <table className="Accounts" >
-                        <thead>
-                            <tr>
-                                <th>Name</th>
-                                <th className="NumberField">Balance</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <AccountCategory title="Accounts" accounts={groups.account.items} sum={groups.account.sum} />
-                            <AccountCategory title="Credit Cards" accounts={groups.cc.items} sum={groups.cc.sum} />
-                            <AccountCategory title="Investments" accounts={groups.invest.items} sum={groups.invest.sum} />
-                        </tbody>
-                        <tfoot>
-                            <tr>
-                                <th>Total</th>
-                                <th className="NumberField">{formatNumber(groups.total, 'R$')}</th>
-                            </tr>
-                        </tfoot>
-                    </table>
-                </GridCell>
-            </GridRow>
-        </Fragment>
-    );
+        return (
+            <Fragment>
+                <GridRow>
+                    <GridCell>
+                        <h2>Accounts</h2>
+                    </GridCell>
+                    <GridCell shrink>
+                        <Link to="/accounts/new">+</Link>
+                    </GridCell>
+                </GridRow>
+                <GridRow>
+                    <GridCell>
+                        <table className="Accounts" >
+                            <thead>
+                                <tr>
+                                    <th>Name</th>
+                                    <th className="NumberField">Balance</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <AccountCategory title="Accounts" accounts={groups.account.items} sum={groups.account.sum} />
+                                <AccountCategory title="Credit Cards" accounts={groups.cc.items} sum={groups.cc.sum} />
+                                <AccountCategory title="Investments" accounts={groups.invest.items} sum={groups.invest.sum} />
+                            </tbody>
+                            <tfoot>
+                                <tr>
+                                    <th>Total</th>
+                                    <th className="NumberField">{formatNumber(groups.total, 'R$')}</th>
+                                </tr>
+                            </tfoot>
+                        </table>
+                    </GridCell>
+                </GridRow>
+            </Fragment>
+        );
+    }
 }
 
 /**
