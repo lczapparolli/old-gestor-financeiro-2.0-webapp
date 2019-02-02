58c6fecfdf57c5b3d5b44fd3c3c92a0ee2e437f3
Luis Carlos <lczapparolli@gmail.com>
2019-02-02 14:46:08 -0200

    Implementing new account action

--- src/pages/AccountPage.js
+++ src/pages/AccountPage.js
@@ -1,16 +1,31 @@
 //Libs
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 //Form
 import AccountForm from '../forms/AccountForm';
+import accountsController from '../controllers/AccountsController';
+//Controller
 
 class AccountPage extends Component {
+    constructor(props) {
+        super(props);
+        //Bindings
+        this.handleSubmit = this.handleSubmit.bind(this);
+        //State
+        this.state = {
+            success: false
+        };
+    }
 
-    handleSubmit() {
-
+    async handleSubmit(account) {
+        await accountsController.addAccount(account);
+        this.setState({ success: true });
     }
 
     render() {
+        if (this.state.success)
+            return <Redirect to="/dashboard" />;
+        //Implementar o carregamento de uma conta para edição
         return (
             <div>
                 <h1>Account</h1>
