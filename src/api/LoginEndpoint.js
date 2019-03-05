598676f9baca77a43c201e7185ced0531f9b8fb0
Luis Carlos <lczapparolli@gmail.com>
2019-03-05 14:57:12 -0300

    Mocking user login for development
    - Mocked login endpoint when using development env

--- src/api/LoginEndpoint.js
+++ src/api/LoginEndpoint.js
@@ -1,11 +1,17 @@
 import axios from 'axios';
 
+const { NODE_ENV } = process.env;
+
 class LoginEndpoint {
     login(email, password) {
-        return axios.post('users/login', { email, password })
-            .then(result => {
-                return result.data;
-            });
+        if (NODE_ENV === 'development') {
+            return Promise.resolve({ token: 'valid token' });
+        } else {
+            return axios.post('users/login', { email, password })
+                .then(result => {
+                    return result.data;
+                });
+        }
     }
 }
 
