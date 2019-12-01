2a517da79f09dbca3c481c811b469eeef7511867
Luis Carlos Zapparolli Neto <lczapparolli@gmail.com>
2019-12-01 20:48:07 -0300

    Creating AccountsPeriod model

---
+++ src/models/AccountPeriod.js
@@ -0,0 +1,53 @@
+/**
+ * @typedef {import('./Account').default} Account
+ */
+
+/**
+ * Account data related to a period
+ */
+class AccountPeriod {
+
+    /**
+     * @type {Number} AccountPeriod id
+     */
+    id;
+
+    /**
+     * @type {Number} Account id
+     */
+    accountId;
+
+    /**
+     * @type {Account} Account object related
+     */
+    account;
+
+    /**
+     * @type {Numeric} Period related
+     */
+    period;
+
+    /**
+     * @type {Number} Initial balance, equals to balance of previous period
+     */
+    initialBalance;
+
+    /**
+     * @type {Number} Final balance, summing all movements in period
+     */
+    balance;
+
+    /**
+     * Creates a new instance of an AccountPeriod object
+     * @param {Number} accountId Related account id
+     * @param {Number} period Related period 
+     * @param {Number} initialBalance Initial balance in period 
+     */
+    constructor(accountId, period, initialBalance) {
+        this.accountId = this.accountId;
+        this.period = period;
+        this.initialBalance = initialBalance;
+    }
+}
+
+export default AccountPeriod;
